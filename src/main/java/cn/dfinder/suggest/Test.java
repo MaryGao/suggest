c0e9ec7ad225b136be12f86de0c0ee4b071b7045
tilney <zechuanyoung@gmail.com>
2014-10-11 11:14:26 +0800

    新的测试用例

--- src/main/java/cn/dfinder/suggest/Test.java
+++ src/main/java/cn/dfinder/suggest/Test.java
@@ -19,15 +19,27 @@ import org.apache.lucene.util.Version;
 
 public class Test {
 	
-	public static void main(String[] args) throws IOException, BadHanyuPinyinOutputFormatCombination {
+	public static void main(String[] args) throws IOException,
+			BadHanyuPinyinOutputFormatCombination {
+		// 新建索引目录..../suggest/index
 		Suggestor sug = new Suggestor(FSDirectory.open(new File("index")));
-		
- // 		sug.updateSuggestIndex(new BufferedReader(new InputStreamReader(new FileInputStream(new File("indicator.dic")))), new IndexWriterConfig(Version.LUCENE_48, null), true);
-		
-		System.out.println(Arrays.toString(sug.suggestSimilar("教", 10, 0.5f)));
-		
+		// 新建索引文件
+		//sug.updateSuggestIndex(new BufferedReader(new InputStreamReader(new FileInputStream(new File("indicator.dic")))), new IndexWriterConfig(Version.LUCENE_48, null), true);
+
+		//提高精度可以提高正确率，长词（7个字以上）下0.5f左右出的结果比较合适
+		// 中文测试
+		System.out.println("****中文测试以及拼音首字母混合补全****");
+		System.out.println(Arrays.toString(sug.suggestSimilar("教y", 10, 0.1f)));
+		System.out.println(Arrays.toString(sug.suggestSimilar("教yu", 10, 0.1f)));
+
+		// 首字母
+		System.out.println("****拼音首字母补全****");
+		System.out.println(Arrays.toString(sug.suggestSimilar("hl", 10, 0.1f)));
+		// 拼音
+		System.out.println("****拼音补全****");
+		System.out.println(Arrays.toString(sug.suggestSimilar("senlin", 10, 0.1f)));
+
 		sug.close();
-		
 	}
 
 
